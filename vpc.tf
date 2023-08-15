0da5c826eb381a591964b08eb927af5e6adfdb48
oluwafemiayo <olufemi123ayo@gmail.com>
2023-08-15 22:29:19 +0300

    added new resource

--- vpc.tf
+++ vpc.tf
@@ -84,7 +84,7 @@ resource "aws_subnet" "private_app_subnet_az1" {
   map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = "private app subnet az1 cidr"
+    Name    = "private app subnet az1"
   }
 }
 
@@ -97,7 +97,7 @@ resource "aws_subnet" "private_app_subnet_az2" {
   map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = "private app subnet az2 cidr",
+    Name    = "private app subnet az2",
   }
 }
 
@@ -110,7 +110,7 @@ resource "aws_subnet" "private_data_subnet_az1" {
   map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = "private data subnet az1 cidr"
+    Name    = "private data subnet az1"
   }
 }
 
@@ -123,6 +123,6 @@ resource "aws_subnet" "private_data_subnet_az2" {
   map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = "private data subnet az2 cidr"
+    Name    = "private data subnet az2"
   }
 }
